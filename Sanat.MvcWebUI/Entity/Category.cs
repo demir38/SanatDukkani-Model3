2935d4e99548d4d8753bd38d181a2dbed117a9bb
demir38 <dmrrrr.mstf@gmail.com>
2018-04-13 19:47:20 +0300

    Sanat

--- Sanat.MvcWebUI/Entity/Category.cs
+++ Sanat.MvcWebUI/Entity/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +10,11 @@ namespace Sanat.MvcWebUI.Entity
     public class Category
     {
         public int Id { get; set; }
-
+        //Data annotations
+        [DisplayName("Katagori Adı")]
+        [StringLength(maximumLength:20,ErrorMessage ="en fazla 20 karakter girebilirsiniz. ")]
         public string Name { get; set; }
+        [DisplayName("Açıklama")]
         public string Description { get; set; }
 
         public List<Product> Products { get; set; }
